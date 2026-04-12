e25c92d4475c2f73a0b6261c55fea92e18705620
Josue <josueamiguel2181@gmail.com>
2026-04-12 18:00:13 -0400

    adding final version

--- Lab7/Lab7.js
+++ Lab7/Lab7.js
@@ -1,19 +1,27 @@
-function searchBook() {
+async function searchBook() {
     const book = document.getElementById("search").value;
     const bookName = book.trim().replace(/ /g, '+');
     let bookTitle = [];
     let authorName = [];
 
-    fetch(`https://openlibrary.org/search.json?title=${bookName}`)
-        .then((result) => result.json())
-        .then((resultJson) => {
-            resultJson.docs.forEach((book) => {
-                bookTitle.push(book.title);
-                authorName.push(book.author_name);
-            });
-            for (let i = 0; i < bookTitle.length; i++) {
-            document.getElementById("book_name").innerHTML += `<li>${bookTitle[i]}</li>`;
-            document.getElementById("author").innerHTML += `<li>${authorName[i]}</li>`;
-            }
-        });
+    document.getElementById("book_name").innerHTML = "";
+    document.getElementById("author").innerHTML = "";
+
+   
+    document.getElementById("loading").style.display = "block";
+
+    const resultJson = await fetch(`https://openlibrary.org/search.json?title=${bookName}`).then((result) => result.json()); // ✅ Fixed variable name
+
+
+    resultJson.docs.forEach((book) => {
+        bookTitle.push(book.title);
+        authorName.push(book.author_name);
+    });
+
+    for (let i = 0; i < bookTitle.length; i++) {
+        document.getElementById("book_name").innerHTML += `<li>${bookTitle[i]}</li>`;
+        document.getElementById("author").innerHTML += `<li>${authorName[i]}</li>`;
+    }
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("row_header").style.display= "flex";
 }
